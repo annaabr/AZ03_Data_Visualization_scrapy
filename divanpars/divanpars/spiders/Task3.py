e97dcc0e1684b3510064a6053b878a26f7e58706
annaabr <annaabramyan2013@gmail.com>
2024-10-08 13:51:21 +0300

    all done

--- divanpars/divanpars/spiders/Task3.py
+++ divanpars/divanpars/spiders/Task3.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Загрузка данных из CSV файла, пропуская первую строку
+# Загрузка данных из CSV файла
 df = pd.read_csv('price.csv')
 
-df['price'] = df['price'].str.replace(' ', '')
-print(df.head())
+# Удаление пробелов и преобразование цен в целые числа
+df['price'] = df['price'].str.replace(' ', '').astype(int)
 
-# scrapy crawl divannewpars
+# Вычисление средней цены
+average_price = df['price'].mean()
+print(f'Средняя цена на диваны: {average_price}')
+
+# Построение гистограммы
+plt.figure(figsize=(10, 6))
+plt.hist(df['price'], bins=20, color='skyblue', edgecolor='black')
+plt.title('Гистограмма цен на диваны')
+plt.xlabel('Цена')
+plt.ylabel('Количество диванов')
+plt.grid(axis='y', alpha=0.75)
+plt.show()
